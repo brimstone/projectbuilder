8daa1465b37a325acc1d0898bea3371105e35e6d
Matt Robinson <brimstone@the.narro.ws>
2016-08-16 21:13:12 -0400

    Update go sample just a tad

--- samples/go-docker-simple/main.go
+++ samples/go-docker-simple/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var COMMITHASH = "dev"
+var BUILDDATETIME = "now"
+
 func main() {
-	log.Println(http.Get("https://google.com"))
+	log.Println("COMMITHASH:", COMMITHASH, "BUILDDATETIME:", BUILDDATETIME)
+	resp, _ := http.Get("http://echoip.com")
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println(string(body))
 }
